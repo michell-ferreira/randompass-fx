6b494c4973e3d8a9d80bdb2bd8096cb4636fa364
MicheL-Frr <216165977+MicheL-Frr@users.noreply.github.com>
2025-12-17 08:19:35 -0300

    feat: substitui interface de console por interface gráfica JavaFX

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,44 @@
 package org.example;
 
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.example.service.GeradorDeSenha;
 
-import java.util.Scanner;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite o comprimento da senha que deseja: ");
-        int comprimentoDaSenha = scanner.nextInt();
-        String senha = GeradorDeSenha.gerarSenha(comprimentoDaSenha);
-
-        System.out.println("Senha gerada: " + senha);
-
-
+        launch(args);
     }
 
 
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Gerador de Senhas");
+
+        Label labelTamanhoSenha = new Label("Tamanho da senha: ");
+        TextField fieldTamanhoSenha = new TextField();
+        fieldTamanhoSenha.setPromptText("Digite o tamanho (ex: 12)");
+
+        Label labelSenhaGerada = new Label();
+        TextField fieldSenhaGerada = new TextField();
+        fieldSenhaGerada.setEditable(false); // usuário não consegue mexer na senha gerada
+
+        Button buttonGerarSenha = new Button("Gerar senha");
+        buttonGerarSenha.setOnAction(e -> {
+            int tamanhoSenha = Integer.parseInt(fieldTamanhoSenha.getText());
+            String senha = GeradorDeSenha.gerarSenha(tamanhoSenha);
+            fieldSenhaGerada.setText(senha);
+        });
+
+        VBox vBox = new VBox(labelTamanhoSenha,fieldTamanhoSenha, buttonGerarSenha, labelSenhaGerada, fieldSenhaGerada);
+        Scene scene = new Scene(vBox, 250, 200);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
