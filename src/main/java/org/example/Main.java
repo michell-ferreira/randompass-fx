3af151b0f2bde0d3ec806b96d016cfff626d62cc
MicheL-Frr <216165977+MicheL-Frr@users.noreply.github.com>
2025-12-17 08:54:01 -0300

    feat: implementa validação de entrada e restrição de tamanho mínimo (>3)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,34 +9,48 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.example.service.GeradorDeSenha;
 
-
 public class Main extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
-
 
     @Override
     public void start(Stage stage) {
         stage.setTitle("Gerador de Senhas");
 
-        Label labelTamanhoSenha = new Label("Tamanho da senha: ");
+        Label labelTamanhoSenha = new Label("Tamanho da senha:");
         TextField fieldTamanhoSenha = new TextField();
         fieldTamanhoSenha.setPromptText("Digite o tamanho (ex: 12)");
 
-        Label labelSenhaGerada = new Label();
+        Label labelErro = new Label();
+
         TextField fieldSenhaGerada = new TextField();
-        fieldSenhaGerada.setEditable(false); // usuário não consegue mexer na senha gerada
+        fieldSenhaGerada.setEditable(false);
 
         Button buttonGerarSenha = new Button("Gerar senha");
         buttonGerarSenha.setOnAction(e -> {
-            int tamanhoSenha = Integer.parseInt(fieldTamanhoSenha.getText());
-            String senha = GeradorDeSenha.gerarSenha(tamanhoSenha);
-            fieldSenhaGerada.setText(senha);
+            try {
+                int tamanhoSenha = Integer.parseInt(fieldTamanhoSenha.getText());
+
+                if (tamanhoSenha <= 3) {
+                    labelErro.setText("Digite um número maior que 3.");
+                    fieldSenhaGerada.clear();
+                    return;
+                }
+
+                String senha = GeradorDeSenha.gerarSenha(tamanhoSenha);
+                fieldSenhaGerada.setText(senha);
+                labelErro.setText("");
+
+            } catch (NumberFormatException ex) {
+                labelErro.setText("Digite um número válido maior que 3 (ex: 12).");
+                fieldSenhaGerada.clear();
+            }
         });
 
-        VBox vBox = new VBox(labelTamanhoSenha,fieldTamanhoSenha, buttonGerarSenha, labelSenhaGerada, fieldSenhaGerada);
-        Scene scene = new Scene(vBox, 250, 200);
+        VBox vBox = new VBox(10, labelTamanhoSenha, fieldTamanhoSenha, buttonGerarSenha, labelErro, fieldSenhaGerada);
+        Scene scene = new Scene(vBox, 300, 200);
         stage.setScene(scene);
         stage.show();
     }
